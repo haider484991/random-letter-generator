6eb7786d899e4e99e5411ec65ac8226582df794e
Haider Ali <haider484991@gmail.com>
2025-03-11 20:04:30 +0500

    Refactor Vercel Analytics integration and update .gitignore

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
-import { Analytics } from '@vercel/analytics/react';
+import VercelAnalytics from "../components/VercelAnalytics";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -78,7 +78,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Analytics />
+        <VercelAnalytics />
       </body>
     </html>
   );
